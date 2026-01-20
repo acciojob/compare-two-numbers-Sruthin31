ce64d8c4aea1020b11c2d192c7b01896686ca31c
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-01-20 13:51:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function compareNumbers(){
+const input1 = Number(prompt("Enter any number"));
+const input2 = Number(prompt("Enter any number"));
+
+if(input1 === input2){
+	alert("True");
+}
+else{
+	alert("False");
+}
+}
+compareNumbers();
